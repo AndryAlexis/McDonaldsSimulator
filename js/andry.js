9f0fe888af44cff8e1bf26261eb56554be4442be
AndryAlexis <andry.html@gmail.com>
2024-10-06 13:17:17 +0200

    Testing

--- js/andry.js
+++ js/andry.js
@@ -1,4 +1,78 @@
+import { products } from "./data/data";
+
 const getData = () => products
+
+const createNewCartProductElement = () => {
+            // Create the cartProduct
+            cartProduct = document.createElement('article')
+            cartProduct.classList.add('cart-product')
+            cartProduct.setAttribute('data-id', product.id)
+            cartProduct.innerHTML = `
+                <p>
+                    ${product.name} - ${product.price}€ x <span class="quantity">1</span>
+                </p>
+                <div>
+                    <button class="delete">Eliminar</button>
+                    <button class="plus">+</button>
+                    <button class="minus">-</button>
+                </div>
+            `
+            cartMenu.querySelector('#selected-products').append(cartProduct)
+
+            quantity = 1;
+            cartTotal.textContent = `Total: ${total += product.price}€`;
+
+            // addEventListeners to Buttons
+            cartProduct.querySelector('button.delete').addEventListener('click', () => {
+                cartProduct.remove()
+                cartTotal.textContent = `Total: ${total -= product.price * quantity}€`;
+            })
+            cartProduct.querySelector('button.plus').addEventListener('click', () => {
+                if (quantity < product.stock) {
+                    cartProduct.querySelector('.quantity').textContent = `${++quantity}`
+                    cartTotal.textContent = `Total: ${total += product.price}€`;
+                }
+            })
+            cartProduct.querySelector('button.minus').addEventListener('click', () => {
+                if (quantity > 1) {
+                    cartProduct.querySelector('.quantity').textContent = `${--quantity}`
+                    cartTotal.textContent = `Total: ${total -= product.price}€`;
+                }
+            })
+}
+
+const onClickButtonBehavior = ({ target : button, id}) => {
+    // Product info
+    const id = button.closest('.product').dataset.id
+    const product = products.find(product => product.id == id)
+    let quantity = undefined;
+
+    button.addEventListener('click', () => {
+        // Find cartProduct, if not found cartProduct = undef
+        const cartProducts = cartMenu.querySelectorAll('.cart-product')
+        let cartProduct = Array.from(cartProducts)
+            .find(cartProduct => cartProduct.dataset.id == product.id)
+
+        if (cartProduct) {
+            // Increase quantity and total 
+            quantity = parseInt(cartProduct.querySelector('.quantity').textContent)
+            if (quantity < product.stock) {
+                cartProduct.querySelector('.quantity').textContent = `${++quantity}`
+                cartTotal.textContent = `Total: ${total += product.price}€`;
+            }
+        } else {
+
+        }
+    })
+}
+
+const createButtonElement = (data) => {
+    const button = document.createElement('button')
+
+    button.addEventListenerU('click', (event) => onClickButtonBehavior(event, data))
+
+    return button
+}
 
 const createImgElement = (src, alt) => {
     const img = document.createElement('img')
@@ -40,7 +114,7 @@ const createProductElement = (data) => {
     secondLi.textContent = `Stock: ${data.stock}`
     ul.append(firstLi, secondLi)
 
-    const button = document.createElement('button')
+    const button = createButtonElement(data)
     button.textContent = 'Agregar al Carrito'
     
     innerDiv.append(ul, button)
@@ -62,3 +136,34 @@ const createProductElements = (data) => Array.from(data.map(info => createProduc
 
 const productElements = createProductElements(getData())
 addElementsIntoMain(productElements)
+
+
+// Cart Menu
+const cartMenu = document.querySelector('#shopping-menu')
+const cartTotal = cartMenu.querySelector('.total')
+let total = 0;
+
+cartMenu.querySelector('.empty').addEventListener('click', () => {
+    cartMenu.querySelectorAll('.cart-product').forEach(cartProduct => cartProduct.remove())
+    total = 0;
+    cartTotal.textContent = `Total: ${total}€`
+})
+cartMenu.querySelector('.buy').addEventListener('click', () => {
+    const cartProducts = cartMenu.querySelectorAll('.cart-product')
+
+    if (cartProducts.length) {
+        alert("Compra realizada con éxito. !Gracias por tu compra!")
+        cartProducts.forEach(cartProduct => cartProduct.remove())
+        total = 0;
+        cartTotal.textContent = `Total: ${total}€`
+    } else
+        alert("Su carro está vacío")
+})
+
+
+// Add to Cart 
+const addToCartButtons = document.querySelectorAll('.product button')
+for (let button of addToCartButtons) {
+
+    
+}
